f872e89edf2605955d645c0494603595d9e19fdf
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2024-07-27 00:13:57 -0300

    feat(tests/Services/PetServiceTest): implement pet service test for create method and exception.

--- tests/Services/PetServiceTest.php
+++ tests/Services/PetServiceTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Services;
 
 use App\Dtos\Pet\CreatePetDto;
+use App\Exceptions\Pet\Entity\PetAlreadyExistsException;
 use App\Repositories\Memory\BreedRepository;
 use App\Repositories\Memory\PetRepository;
 use App\Services\PetService;
@@ -16,13 +17,13 @@ class PetServiceTest extends TestCase
         parent::setUp();
     }
 
-    public function test_should_create_animal()
+    public function test_should_create_pet()
     {
         $faker = new Faker();
 
         $service = new PetService(new PetRepository(), new BreedRepository);
 
-        $institutionUuid = 'k3210-341-fk3-120-kf2-f3-o';
+        $institutionUuid = 'e486c3e9-7a97-49b3-a2ca-d8e5718f1f8e';
 
         $dto = new CreatePetDto(
             $faker->petCode,
@@ -38,15 +39,43 @@ class PetServiceTest extends TestCase
 
         $service->create($dto);
 
-        $animal = $service->getByPetCode($dto->petCode);
+        $pet = $service->getByPetCode($dto->petCode);
 
-        $this->assertEquals($dto->petCode, $animal->getIdentity()->getValue());
-        $this->assertEquals($dto->petConditionUuid, $animal->getPetCondition()->getUuid());
-        $this->assertEquals($dto->name, $animal->getName());
-        $this->assertEquals($dto->weight, $animal->getWeight());
-        $this->assertEquals($dto->age, $animal->getAge());
-        $this->assertEquals($dto->neutered, $animal->isNeutered());
-        $this->assertEquals($dto->sex, $animal->getSex());
-        $this->assertEquals($dto->breedUuid, $animal->getBreed()->getKey());
+        $this->assertEquals($dto->petCode, $pet->getIdentity()->getValue());
+        $this->assertEquals($dto->petConditionUuid, $pet->getPetCondition()->getUuid());
+        $this->assertEquals($dto->name, $pet->getName());
+        $this->assertEquals($dto->weight, $pet->getWeight());
+        $this->assertEquals($dto->age, $pet->getAge());
+        $this->assertEquals($dto->neutered, $pet->isNeutered());
+        $this->assertEquals($dto->sex, $pet->getSex());
+        $this->assertEquals($dto->breedUuid, $pet->getBreed()->getKey());
+    }
+
+    public function test_should_not_create_pet_with_same_pet_code()
+    {
+      $faker = new Faker();
+      $petCode = '00007';
+
+      $service = new PetService(new PetRepository(), new BreedRepository);
+
+      $institutionUuid = 'k3210-341-fk3-120-kf2-f3-o';
+
+      $dto = new CreatePetDto(
+          $petCode,
+          $faker->name,
+          $faker->weight,
+          $faker->age,
+          $faker->neutered,
+          $faker->sex,
+          $faker->breedUuid,
+          $faker->petConditionUuid,
+          $institutionUuid
+      );
+      
+      $this->expectException(PetAlreadyExistsException::class);
+            
+      $service->create($dto);
+      $service->create($dto);
+
     }
 }
