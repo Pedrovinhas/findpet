6726e2e82f1493f4d8a59930cc6069f0f3a8894f
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2024-07-30 22:53:12 -0300

    refactor(app/Dtos/Pet/UpdatePetDto): add instution uuid.

--- app/Dtos/Pet/UpdatePetDto.php
+++ app/Dtos/Pet/UpdatePetDto.php
@@ -7,13 +7,14 @@ class UpdatePetDto
     public ?PetSitterDto $petSitter = null;
 
     public function __construct(
-        public readonly ?string $petCod = null,
+        public readonly ?string $petCode = null,
         public readonly ?string $name = null,
         public readonly ?float $weight = null,
         public readonly ?int $age = null,
         public readonly ?bool $neutered = null,
         public readonly ?string $sex = null,
         public readonly ?string $breedUuid = null,
+        public readonly ?string $institutionUuid = null,
         public readonly ?string $petConditionUuid = null,
     ) {
     }
